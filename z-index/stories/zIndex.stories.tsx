05c1ffb24ac6c98faa8b940de5805b8769ba5a73
Gregory Quinio <1063530+undefined@users.noreply.github.com>
2022-04-06 14:02:51 +0000

    Push from aaa

--- z-index/stories/zIndex.stories.tsx
+++ z-index/stories/zIndex.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '~/all/dist/all.css';
+import '~/all/src/all.scss';
 import { StyleShowcases } from '@divriots/dockit-react/style-showcases';
 
 export const z_index = () => <StyleShowcases prefix="--aria-z-index" />;
